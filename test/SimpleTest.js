16656bd7294c2fa71785735c249f04f59f5f8994
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-10-30 19:39:21 +0200

    Remove simple tests

--- test/SimpleTest.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-
-describe('canary-tests', function() 
-{
-    it('should always pass the canary test', function() 
-    {
-        expect(true).to.eql(true);
-    });
-});
