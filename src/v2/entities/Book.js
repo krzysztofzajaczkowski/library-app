215dc70658aefa3d5af2210fa69ad8693bc9952a
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-10-31 16:02:30 +0100

    Update v2 to use constructor function and prototype

--- src/v2/entities/Book.js
+++ src/v2/entities/Book.js
@@ -1,54 +1,52 @@
 "use strict";
 
-class Book {
 
-    constructor(number, title, author, releaseYear, price, lastBorrowDate = null, lastBorrower = null) {
-        if (isNaN(price)) {
+function Book(number, title, author, releaseYear, price, lastBorrowDate = null, lastBorrower = null) {
+    if (isNaN(price)) {
+        throw 'InvalidArgumentException';
+    }
+
+    let _Number = number;
+    let _Title = title;
+    let _Author = author;
+    let _ReleaseYear = releaseYear;
+    let _Price = price;
+    let _LastBorrowDate = lastBorrowDate;
+    let _LastBorrower = lastBorrower;
+    let _Borrowed = lastBorrowDate != null && lastBorrower != null;
+
+    Book.prototype.getNumber = () => _Number;
+    Book.prototype.getTitle = () => _Title;
+    Book.prototype.getAuthor = () => _Author;
+    Book.prototype.getReleaseYear = () => _ReleaseYear;
+    Book.prototype.getLastBorrowDate = () => _LastBorrowDate;
+    Book.prototype.getLastBorrower = () => _LastBorrower;
+
+    Book.prototype.setPrice = (newPrice) => {
+        if (isNaN(newPrice)) {
             throw 'InvalidArgumentException';
         }
+        _Price = newPrice;
+    };
+    Book.prototype.getPrice = () => _Price;
 
-        let _Number = number;
-        let _Title = title;
-        let _Author = author;
-        let _ReleaseYear = releaseYear;
-        let _Price = price;
-        let _LastBorrowDate = lastBorrowDate;
-        let _LastBorrower = lastBorrower;
-        let _Borrowed = lastBorrowDate != null && lastBorrower != null;
+    Book.prototype.isAvailable = () => !_Borrowed;
 
-        Book.prototype.getNumber = () => _Number;
-        Book.prototype.getTitle = () => _Title;
-        Book.prototype.getAuthor = () => _Author;
-        Book.prototype.getReleaseYear = () => _ReleaseYear;
-        Book.prototype.getLastBorrowDate = () => _LastBorrowDate;
-        Book.prototype.getLastBorrower = () => _LastBorrower;
+    Book.prototype.lend = newBorrower => {
+        if(_Borrowed) {
+            throw 'BookUnavailableException'; 
+        }
+        _LastBorrowDate = Date.now();
+        _LastBorrower = newBorrower;
+        _Borrowed = true;
+    };
 
-        Book.prototype.setPrice = (newPrice) => {
-            if (isNaN(newPrice)) {
-                throw 'InvalidArgumentException';
-            }
-            _Price = newPrice;
-        };
-        Book.prototype.getPrice = () => _Price;
-
-        Book.prototype.isAvailable = () => !_Borrowed;
-
-        Book.prototype.lend = newBorrower => {
-            if(_Borrowed) {
-               throw 'BookUnavailableException'; 
-            }
-            _LastBorrowDate = Date.now();
-            _LastBorrower = newBorrower;
-            _Borrowed = true;
-        };
-
-        Book.prototype.return = () => {
-            if(!_Borrowed) {
-                throw 'BookAlreadyReturnedException';
-            }
-            _Borrowed = false;
-        };
-    }
+    Book.prototype.return = () => {
+        if(!_Borrowed) {
+            throw 'BookAlreadyReturnedException';
+        }
+        _Borrowed = false;
+    };
 }
 
 module.exports = Book;
